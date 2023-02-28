846783920bc4419cb6c9c23874083aac277918aa
Pradeep782 <74357885+Pradeep782@users.noreply.github.com>
2023-02-27 19:13:09 -0500

    changed commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
+import React from "react";
 
 function App() {
   
